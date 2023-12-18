75402f7730086b30afb03f2b709266bcc3767cd0
Parth Sharma <parth3745@gmail.com>
2023-12-18 20:31:43 +0530

    LoginTest template class added

--- src/main/java/PageObjects/LoginPage.java
+++ src/main/java/PageObjects/LoginPage.java
@@ -1,5 +1,24 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void perFormLogIn(String username, String password) {
+        usernameInput.sendKeys(username);
+    }
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
 
 }
--- src/main/java/Utilities/Initializer.java
+++ src/main/java/Utilities/Initializer.java
@@ -1,3 +1,5 @@
+package Utilities;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
